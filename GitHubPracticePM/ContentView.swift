47206cee61dd771568b9c5be8042c180011d3e54
PMStudent <PMStudent@RS-F251-M02.local>
2021-09-14 13:14:31 -0500

    broke project

---
+++ GitHubPracticePM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubPracticePM
+//
+//  Created by PMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubPracticePM/GitHubPracticePMApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracticePMApp.swift
+//  GitHubPracticePM
+//
+//  Created by PMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracticePMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
